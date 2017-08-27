97f7e972aab1905af944dd85d82d5645340439ec
wwwmonsterlam <wuweilin.william@qq.com>
2017-08-26 23:20:33 -0300

    add two sum

---
+++ src/easy/working/TwoSum1.java
@@ -0,0 +1,25 @@
+package easy.working;
+
+import java.util.HashMap;
+
+public class TwoSum1 {
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            if(nums == null || nums.length == 0) 
+                return null;
+            
+            HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+            
+            for(int i = 0; i < nums.length; i++) {
+                Integer temp = map.get(target - nums[i]);                
+                if(temp != null) {
+                    int[] result = {(int)temp, i};
+                    return result;
+                } else {
+                    map.put(nums[i], i);
+                }
+            }
+            return null;
+        }
+    }
+}
